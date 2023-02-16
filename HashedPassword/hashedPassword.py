4a663e6bc1f7e23b2c1655e77520289d13c65915
Argus Coeus <richieblur@Beast.Beast>
2023-02-16 14:58:30 -0500

    Initialing Message

--- HashedPassword/hashedPassword.py
+++ HashedPassword/hashedPassword.py
@@ -7,8 +7,8 @@ from tkinter import messagebox
 class Hashed(Tk):
     def __init__(self):
        super().__init__()
-       self.height =150
-       self.width = 300
+       self.height =170
+       self.width = 305
        self.x =(self.winfo_screenwidth()//2) - (self.width//2)
        self.y =(self.winfo_screenheight()//2) - (self.height//2)
        self.title("HASHED PASSWORD ")
@@ -16,31 +16,37 @@ class Hashed(Tk):
        self.resizable(False,False)
        self.gui()
     def hashedPassword(self):
-        if(self.var.get() == 1):
-            result = hashlib.md5(self.passwordEntry.get().encode())
-            entry = result.digest()
-            self.label.config(text= entry, font= ('Helvetica 10'),fg="black")
-        elif(self.var.get() == 2):
-            result = hashlib.sha256(self.passwordEntry.get().encode())
-            entry = result.digest()
-            self.label.config(text= entry, font= ('Helvetica 10'),fg="black")
-        elif(self.var.get() == 3):
-            result = hashlib.sha512(self.passwordEntry.get().encode())
-            entry = result.digest()
-            self.label.config(text= entry, font= ('Helvetica 10'),fg="black",width=35)
+        if(not self.passwordEntry.get() ):
+            messagebox.showinfo("Required","Fill the entry box.")
         else:
-            messagebox.showerror("Error","RadioButton Required")
-    
+            if(self.var.get() == 1):
+                result = hashlib.md5(self.passwordEntry.get().encode())
+                entry = result.hexdigest()
+                self.label.insert(END,entry)
+                self.label.config(fg="black",width=35)
+            elif(self.var.get() == 2):
+                result = hashlib.sha256(self.passwordEntry.get().encode())
+                entry = result.hexdigest()
+                self.label.insert(END,entry)
+                self.label.config(fg="black",width=35)
+            elif(self.var.get() == 3):
+                result = hashlib.sha512(self.passwordEntry.get().encode())
+                entry = result.hexdigest()
+                self.label.insert(END,entry)
+                self.label.config(fg="black",width=35)
+            else:
+                messagebox.showerror("Error","RadioButton Required")
     def gui(self): 
         self.var = IntVar()
         self.nameVar = StringVar()
+        self.stringLabel = StringVar()
         self.passwordEntry = Entry(self,width=20,bd=5,font=("", 12))
         self.passwordEntry.config(highlightcolor="white",relief="sunken")
         self.passwordEntry.place(x=40, y =35)
         #########################################################################
         passwordButton = Button(self,text = "Done",bg="#ffffff",font=("",13,"bold"),bd=0,cursor="hand2",
         activebackground="#ffffff",width=10,relief="raised",command=self.hashedPassword)
-        passwordButton.place(x=110, y=110)
+        passwordButton.place(x=80, y=120)
         ############################################################################
         md5Button = Radiobutton(self,text="MD5", variable=self.var, value=1,
                   command="")
@@ -53,7 +59,7 @@ class Hashed(Tk):
         sha543Button.place(x=170, y=10)
         ############################################################################
         
-        self.label = Label( self, text = "", relief=RAISED,height=2,width=35,bg="#ffffff" )
+        self.label = Text( self,height=2,width=35,bg="#ffffff")
         self.label.place(x=10, y=70)
 
             
