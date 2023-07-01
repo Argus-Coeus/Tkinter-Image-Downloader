00ccbdd856635a49ae3e3e45c39b4b503dad0a60
Argus <richard.arthur003@stu.ucc.edu.gh>
2023-07-01 11:53:50 -0400

    boring Script

--- Requests-Py/sample.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-res = requests.get("https://media.istockphoto.com/id/1451079337/photo/customer-review-good-rating-concept-hand-pressing-user-and-five-star-icon-on-visual-screen.webp?s=1024x1024&w=is&k=20&c=QPAM1YxTn6I8Qwp90manpa0ikNcuMEqfLC4Zs0j37qQ=",verify=True)
-
-print(res)
